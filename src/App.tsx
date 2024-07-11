916dfe6f531627e59e5eea1710e5070429bc7864
Jeremia <jeripane@gmail.com>
2024-07-11 18:27:45 +0700

    feat: landing page to development

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./features/guests/LandingPage";
+import LoginPage from "./features/authentication/LoginPage";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LandingPage />}></Route>
+                <Route path="/login" element={<LoginPage />}></Route>
             </Routes>
         </Router>
     );
