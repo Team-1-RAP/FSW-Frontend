92637a309311bf2de4f57825c2d6e7f7aaff6676
Aji Nuansa Sengarie <ansengarie@gmail.com>
2024-07-13 14:50:40 +0700

    feat: responsive mobile login page

--- src/features/authentication/LoginPage.tsx
+++ src/features/authentication/LoginPage.tsx
@@ -20,7 +20,9 @@ const LoginPage: React.FC = () => {
             return false;
         }
         if (!usernamePattern.test(username)) {
-            setError("Username harus huruf kecil dan hanya berisi huruf dan angka");
+            setError(
+                "Username harus huruf kecil dan hanya berisi huruf dan angka"
+            );
             return false;
         }
         if (!password) {
@@ -42,13 +44,16 @@ const LoginPage: React.FC = () => {
         e.preventDefault();
         if (validateInput()) {
             try {
-                const response = await fetch("https://cautious-noelyn-ridho-71c54445.koyeb.app/api/v1/auth/login", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({ username, password }),
-                });
+                const response = await fetch(
+                    "https://cautious-noelyn-ridho-71c54445.koyeb.app/api/v1/auth/login",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ username, password }),
+                    }
+                );
                 if (!response) {
                     setError("Login Gagal");
                 }
@@ -68,28 +73,43 @@ const LoginPage: React.FC = () => {
     return (
         <div className="relative">
             <div className="">
-                <img src={bgLogin} alt="" className="absolute left-0 h-full" />
+                <img
+                    src={bgLogin}
+                    alt=""
+                    className="absolute left-0 object-cover w-full h-screen md:h-full md:w-fit"
+                />
             </div>
-            <div className="flex flex-row w-full h-screen">
-                <div className="relative w-1/2 h-screen">
-                    <div className="absolute inset-0 flex flex-col items-center justify-center mb-40 ">
-                        <div className="w-[75%] space-y-10 text-white">
-                            <p className="text-[59px]">
+            <div className="flex flex-col w-full md:h-screen md:flex-row">
+                <div className="relative w-full h-screen md:w-1/2">
+                    <div className="absolute flex flex-col mb-40 md:items-center md:justify-center left-5 top-4 md:inset-0 ">
+                        <div className="md:space-y-10 space-y-16 text-white md:w-[75%] w-full">
+                            <p className="text-[59px] mr-8 md:mr-0">
                                 Selamat Datang di <b>Simple Bank</b>
                             </p>
-                            <p className="text-[27px]">
-                                <b>Login</b> untuk akses akun <b>Simple Bank</b> mu
+                            <p className="text-[27px] mr-8 md:mr-0">
+                                <b>Login</b> untuk akses akun <b>Simple Bank</b>{" "}
+                                mu
                             </p>
                         </div>
                     </div>
-                    <div className="absolute inset-x-0 bottom-0 flex flex-col items-start justify-end ml-[36px] mb-[52px] text-white">
-                        <p>Simple bank adalah pelaku jasa keuangan terdaftar dan diawasi oleh Otoritas Jasa Keuangan</p>
+                    <div className="absolute inset-x-0 bottom-0 flex flex-col items-start justify-end md:ml-[36px] m-5 md:mb-[52px] text-white space-y-4 md:space-y-0">
+                        <p className="mr-[100px] md:mr-0">
+                            Simple bank adalah pelaku jasa keuangan terdaftar
+                            dan diawasi oleh Otoritas Jasa Keuangan
+                        </p>
                         <p>Copyright © 2024 - Simple Bank</p>
                     </div>
                 </div>
-                <div className="flex flex-col items-center justify-center w-1/2 h-screen">
-                    <img src={logo} alt="Simple Bank" className="w-[194px] mb-[29px]" />
-                    <form onSubmit={handleSubmit} className="w-1/2 space-y-3">
+                <div className="flex flex-col items-center justify-center w-full h-screen md:w-1/2">
+                    <img
+                        src={logo}
+                        alt="Simple Bank"
+                        className="w-[194px] mb-[29px]"
+                    />
+                    <form
+                        onSubmit={handleSubmit}
+                        className="w-11/12 space-y-3 md:w-1/2"
+                    >
                         <div className="relative">
                             <User className="text-[#c4c4c4] absolute left-2 top-3" />
                             <input
@@ -111,11 +131,20 @@ const LoginPage: React.FC = () => {
                                 onChange={(e) => setPassword(e.target.value)}
                                 aria-label="password"
                             />
-                            <div className="absolute right-4 top-3 cursor-pointer" onClick={toggleShowPassword}>
-                                {showPassword ? <Eye className="text-[#c4c4c4]" /> : <EyeOff className="text-[#c4c4c4]" />}
+                            <div
+                                className="absolute cursor-pointer right-4 top-3"
+                                onClick={toggleShowPassword}
+                            >
+                                {showPassword ? (
+                                    <Eye className="text-[#c4c4c4]" />
+                                ) : (
+                                    <EyeOff className="text-[#c4c4c4]" />
+                                )}
                             </div>
                         </div>
-                        {error && <div className="text-red-500 text-sm">{error}</div>}
+                        {error && (
+                            <div className="text-sm text-red-500">{error}</div>
+                        )}
                         <p className="flex justify-end text-[#153193]">
                             <span>
                                 forgot your <b>password?</b>
@@ -123,7 +152,10 @@ const LoginPage: React.FC = () => {
                         </p>
                         <div className="border border-[#6C8FEE] w-full"></div>
                         <div className="flex justify-center">
-                            <button type="submit" className="mb-[100px] mt-[111px] bg-gradient-to-tr to-[#2AF0FA] from-[#0C32FB] py-[10px] px-[60px] rounded-[12px] text-white text-base border-[#5375EC] border-2">
+                            <button
+                                type="submit"
+                                className="mb-[100px] mt-[111px] bg-gradient-to-tr to-[#2AF0FA] from-[#0C32FB] py-[10px] px-[60px] rounded-[12px] text-white text-base border-[#5375EC] border-2"
+                            >
                                 Login
                             </button>
                         </div>
