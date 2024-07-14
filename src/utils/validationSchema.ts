b7c1fba15f138f9ae705521bd3798e3811f52b73
SahidJafar <sahid11.sj@gmail.com>
2024-07-14 22:19:18 +0700

    fix: uncomment regex password

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -5,6 +5,6 @@ export const loginValidationSchema = Yup.object({
     .matches(/^[a-z][a-z0-9]*$/, "Username harus huruf kecil dan hanya berisi huruf dan angka")
     .required("Username wajib diisi!"),
   password: Yup.string()
-    // .matches(/^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, "Kombinasi password belum sesuai")
+    .matches(/^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, "Kombinasi password belum sesuai")
     .required("Password harus diisi"),
 })
