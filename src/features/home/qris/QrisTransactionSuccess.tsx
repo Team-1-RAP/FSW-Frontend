59ae01521717726e0f6b94affc4d64247ef5bdea
Andi <{an.ekanugraha@gmail.com}>
2024-08-17 18:17:00 +0700

    add api page success

--- src/features/home/qris/QrisTransactionSuccess.tsx
+++ src/features/home/qris/QrisTransactionSuccess.tsx
@@ -1,9 +1,11 @@
 import { Check } from "react-feather";
 import Button from "../../../components/fragments/Authentication/Button";
 import { useNavigate } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 export const QrisTransactionSuccess = () => {
     const navigate = useNavigate();
+    const location = useLocation();
   return (
     <div className="flex flex-col gap-11 items-center w-[389px] pb-10 pt-10">
       <div className="font-medium text-3xl text-center">
@@ -15,13 +17,13 @@ export const QrisTransactionSuccess = () => {
       </div>
       <div className="text-center">
         <p>
-          Anda telah membayar sebesar <b>Rp500.000</b>
+          Anda telah membayar sebesar <b>Rp{location.state.data.data.amount}</b>
         </p>
         <p>
-          melalui fitur QRIS kepada <b>Toko Madura</b>
+          melalui fitur QRIS kepada <b>{location.state.data.data.name}</b>
         </p>
       </div>
-      <Button className="bg-primary" onClick={() => navigate("")}>
+      <Button className="bg-primary" onClick={() => navigate("/home")}>
         Kembali ke home
       </Button>
     </div>
